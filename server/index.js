1495cf3615b9d51a0bcb6dc95053b77b3b518c4d
Brian Kim <brianjunewookim@gmail.com>
2019-05-02 16:35:30 -0700

    Implement photos and nearby homes components

--- server/index.js
+++ server/index.js
@@ -6,8 +6,6 @@ const axios = require('axios');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(morgan('dev'));
-
 app.get('/', (req, res) => res.redirect(`/bookings/${Math.floor(Math.random()* 100)}`));
 
 app.use('/bookings/:roomid', express.static(path.join(__dirname, '../public')));
@@ -19,29 +17,29 @@ app.get('/photosandcomments/:accommodationid', (req, res) => {
     });
 });
 
-app.get('/bookings/:accommodationid/reserve', (req, res) => {
-  axios.get(`http://localhost:3003/bookings/${req.params.accommodationid}/reserve`)
-    .then(response => {
-      res.send(response.data);
-    });
-});
+// app.get('/bookings/:accommodationid/reserve', (req, res) => {
+//   axios.get(`http://localhost:3003/bookings/${req.params.accommodationid}/reserve`)
+//     .then(response => {
+//       res.send(response.data);
+//     });
+// });
 
-app.get('/bookings/:accommodationid/reserve/:startDate&:endDate', (req, res) => {
-  axios.get(`http://localhost:3003/bookings/${req.params.accommodationid}/reserve/${req.params.startDate}&${req.params.endDate}`)
-    .then(response => {
-      res.send(response.data);
-    });
-});
+// app.get('/bookings/:accommodationid/reserve/:startDate&:endDate', (req, res) => {
+//   axios.get(`http://localhost:3003/bookings/${req.params.accommodationid}/reserve/${req.params.startDate}&${req.params.endDate}`)
+//     .then(response => {
+//       res.send(response.data);
+//     });
+// });
 
-app.get('/abodes/:abode_id/reviews', (req, res) => {
-  axios.get(`http://localhost:3002/abodes/${req.params.abode_id}/reviews`)
-    .then(response => {
-      res.send(response.data);
-    });
-});
+// app.get('/abodes/:abode_id/reviews', (req, res) => {
+//   axios.get(`http://localhost:3002/abodes/${req.params.abode_id}/reviews`)
+//     .then(response => {
+//       res.send(response.data);
+//     });
+// });
 
-app.get('/homes', (req, res) => {
-  axios.get(`http://ec2-13-57-193-150.us-west-1.compute.amazonaws.com:3004/homes`)
+app.get('/homes/:accommodationid/nearby', (req, res) => {
+  axios.get(`http://ec2-54-183-57-67.us-west-1.compute.amazonaws.com/homes/${req.params.accommodationid}/nearby`)
     .then(response => {
       res.send(response.data);
     });
