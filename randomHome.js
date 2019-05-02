f53d6526f7f5eeb09cb1f077d886f4ae58152df8
Brian Kim <brianjunewookim@gmail.com>
2019-05-02 16:17:56 -0700

    Remove stress test files

--- randomHome.js
+++
@@ -1,7 +0,0 @@
-function randomHome(userContext, events, done) {
-  let id = Math.ceil(Math.random() * 10000000);
-  userContext.vars.id = id;
-  return done();
-}
-
-module.exports = { randomHome };
